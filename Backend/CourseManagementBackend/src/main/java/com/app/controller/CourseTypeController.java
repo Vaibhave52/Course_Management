13dd1d63d6e2acf942b839c77c471ff3a16b8d14
Vaibhav <vaibhave52@gmail.com>
2025-08-25 12:04:58 +0530

    Course_Type Related Crud Operation

---
+++ Backend/CourseManagementBackend/src/main/java/com/app/controller/CourseTypeController.java
@@ -0,0 +1,96 @@
+package com.app.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.app.dto.CourseTypeDto;
+import com.app.entity.CourseType;
+import com.app.responsemessage.ApiResponse;
+import com.app.service.CourseTypeService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("/coursetype")
+@Slf4j
+public class CourseTypeController {
+
+	@Autowired
+	private CourseTypeService courseTypeService;
+
+	// create
+
+	@PostMapping
+	public ResponseEntity<CourseTypeDto> createCourseType(@RequestBody CourseTypeDto courseTypeDto) {
+
+		log.info("CourseTypeController Layer Description  : {}, Tittle :{}", courseTypeDto.getDescription(),
+				courseTypeDto.getTitle());
+
+		return new ResponseEntity<CourseTypeDto>(courseTypeService.createCourseType(courseTypeDto), HttpStatus.CREATED);
+
+	}
+
+	// update
+
+	@PutMapping("/{courseTypeId}")
+	public ResponseEntity<CourseTypeDto> updateCourseType(@RequestBody CourseTypeDto courseTypeDto,
+			@PathVariable(name = "courseTypeId") Long courseTypeId) {
+
+		log.info("CourseTypeController Layer {}", courseTypeId);
+
+		return new ResponseEntity<CourseTypeDto>(courseTypeService.updateCourseType(courseTypeId, courseTypeDto),
+				HttpStatus.OK);
+
+	}
+
+	// getAll
+
+	@GetMapping()
+	public ResponseEntity<List<CourseTypeDto>> getAllCourseType() {
+
+		log.info("CourseTypeController Layer get All CourseType");
+
+		return new ResponseEntity<List<CourseTypeDto>>(courseTypeService.getAllCourseTypes(), HttpStatus.OK);
+
+	}
+
+	// getById
+
+	@GetMapping("/{courseTypeId}")
+	public ResponseEntity<CourseTypeDto> getCourseTypeById(@PathVariable(name = "courseTypeId") Long courseTypeId) {
+
+		log.info("In controller layer coursetype id is {}", courseTypeId);
+
+		return new ResponseEntity<CourseTypeDto>(courseTypeService.getCourseTypeById(courseTypeId), HttpStatus.OK);
+
+	}
+
+	// delete
+
+	@DeleteMapping("/{courseTypeId}")
+	public ResponseEntity<ApiResponse> deleteCourseTypeById(@PathVariable(name = "courseTypeId") Long courseTypeId) {
+
+		courseTypeService.deleteCourseType(courseTypeId);
+
+		log.info("CourseTypeController Layer {}", courseTypeId);
+
+		ApiResponse deleteMeassage = ApiResponse.builder()
+				.message("Coursetype deleted Successfully with id :" + courseTypeId).status(HttpStatus.OK).build();
+
+		return new ResponseEntity<ApiResponse>(deleteMeassage, HttpStatus.OK);
+
+	}
+
+}
---
+++ Backend/CourseManagementBackend/src/main/java/com/app/dto/CourseSummaryDto.java
@@ -0,0 +1,21 @@
+package com.app.dto;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class CourseSummaryDto {
+
+	private Long courseId;
+
+	private String name;
+}
