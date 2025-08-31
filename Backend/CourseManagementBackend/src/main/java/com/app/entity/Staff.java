48d7b5d48ee1f06bd5d9515a238930166a4078ba
Vaibhav <vaibhave52@gmail.com>
2025-08-31 20:47:57 +0530

    deleted role modified course in Session to course module and remove column staff from session

--- Backend/CourseManagementBackend/src/main/java/com/app/entity/Staff.java
+++ Backend/CourseManagementBackend/src/main/java/com/app/entity/Staff.java
@@ -1,9 +1,6 @@
 package com.app.entity;
 
-
-
 import com.app.entity.enums.StaffType;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -11,8 +8,6 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -29,31 +24,27 @@ import lombok.ToString;
 @Setter
 @Builder
 @ToString
-public class Staff  {
-	
+public class Staff {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "staff_id")
 	private Long staffId;
-	
-	@Column(name = "name",nullable = false,length = 30)
+
+	@Column(name = "name", nullable = false, length = 30)
 	private String name;
-	
-	@Column(name = "password",nullable = false,length = 30)
+
+	@Column(name = "password", nullable = false, length = 30)
 	private String password;
-	
-	@Column(name = "mobile_no",nullable = false,length = 13,unique = true)
+
+	@Column(name = "mobile_no", nullable = false, length = 13, unique = true)
 	private String mobileNo;
-	
-	@Column(name = "email",nullable = false,length = 30,unique = true)
+
+	@Column(name = "email", nullable = false, length = 30, unique = true)
 	private String email;
-	
+
 	@Enumerated(EnumType.STRING)
 	private StaffType staffType;
-	
-	@ManyToOne
-	@JoinColumn(name = "role_id")
-	private Role role;
 
 	public String getPassword() {
 		return this.password;
